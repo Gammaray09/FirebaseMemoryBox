b745ff3743f9e61fec4425fac8e6049936957680
Aashman Sharma <90787261+Gammaray09@users.noreply.github.com>
2022-10-13 10:56:59 -0500

    Update FirebaseHelper.java

--- app/src/main/java/com/example/firebasememorybox/FirebaseHelper.java
+++ app/src/main/java/com/example/firebasememorybox/FirebaseHelper.java
@@ -43,16 +43,23 @@ import java.util.concurrent.Executor;
  * our other classes clean of the firebase code and also avoid having to update firebase code
  * in many places.  This is MUCH more efficient and less error prone.
  */
+
+
 public class FirebaseHelper {
     public final String TAG = "Denna";
     private static String uid = null;      // var will be updated for currently signed in user
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    private ArrayList<Memory> myMemories;
+
     // we don't need this yet
     // private ArrayList<Memory> myItems = new ArrayList<>();
 
 
     public FirebaseHelper() {
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        myMemories = new ArrayList<>();
     }
 
 
@@ -65,31 +72,154 @@ public class FirebaseHelper {
         this.uid = null;
     }
 
+
     public void attachReadDataToUser() {
+        // This is necessary to avoid the issues we ran into with data displaying before it
+        // returned from the asynch method calls
+        if (mAuth.getCurrentUser() != null) {
+            uid = mAuth.getUid();
+            readData(new FirestoreCallback() {
+                @Override
+                public void onCallback(ArrayList<Memory> memoryList) {
+                    Log.d(TAG, "Inside attachReadDataToUser, onCallback " + memoryList.toString());
+                }
+            });
+        }
+        else {
+            Log.d(TAG, "No one logged in");
+        }
 
     }
 
 
+    //adds puts user in database and give them a folder that is matched with their account
     public void addUserToFirestore(String name, String newUID) {
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", name);
+        // Add a new document with a docID = to the authenticated user's UID
+        db.collection("users").document(newUID)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(TAG, name + "'s user account added");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding user account", e);
+                    }
+                });
+    }
+
+
+
+    public void addData(Memory m) {
+        // add Memory m to the database
+        // this method is overloaded and incorporates the interface to handle the asynch calls
+        addData(m, new FirestoreCallback() {
+            @Override
+            public void onCallback(ArrayList<Memory> myList) {
+                Log.i(TAG, "Inside addData, onCallback :" + myMemories.toString());
+            }
+        });
+    }
+
+    private void addData(Memory m, FirestoreCallback firestoreCallback) {
+        db.collection("users").document(uid).collection("myMemoryList")
+                .add(m)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        // This will set the docID key for the Memory that was just added.
+                        db.collection("users").document(uid).collection("myMemoryList").
+                                document(documentReference.getId()).update("docID", documentReference.getId());
+                        Log.i(TAG, "just added " + m.getName());
+                        readData(firestoreCallback);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+
+
+
+    public ArrayList<Memory> getMemoryArrayList() {
+        return myMemories;
+    }
+
+
+    public void editData(Memory m) {
+        // edit Memory m to the database
+        // this method is overloaded and incorporates the interface to handle the asynch calls
+        editData(m, new FirestoreCallback() {
+            @Override
+            public void onCallback(ArrayList<Memory> myList) {
+                Log.i(TAG, "Inside editData, onCallback " + myList.toString());
+            }
+        });
+    }
+    private void editData(Memory m, FirestoreCallback firestoreCallback) {
+        String docId = m.getDocID();
+        db.collection("users").document(uid).collection("myMemoryList")
+                .document(docId)
+                .set(m)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.i(TAG, "Success updating document");
+                        readData(firestoreCallback);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i(TAG, "Error updating document", e);
+                    }
+                });
+    }
+
+
+
+    public void deleteData(Memory m) {
+        // delete item w from database
+        // this method is overloaded and incorporates the interface to handle the asynch calls
+        deleteData(m, new FirestoreCallback() {
+            @Override
+            public void onCallback(ArrayList<Memory> myList) {
+                Log.i(TAG, "Inside deleteData, onCallBack" + myList.toString());
+            }
+        });
 
     }
 
-    public void addData(Memory wish) {
-
+    private void deleteData(Memory m, FirestoreCallback firestoreCallback) {
+        // delete item w from database
+        String docId = m.getDocID();
+        db.collection("users").document(uid).collection("myMemoryList")
+                .document(docId)
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.i(TAG, m.getName() + " successfully deleted");
+                        readData(firestoreCallback);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i(TAG, "Error deleting document", e);
+                    }
+                });
     }
 
-// we will uncomment later
-//    public ArrayList<Memory> getWishListItems() {
-//
-//    }
-
-    public void editData(Memory w) {
-
-    }
-
-    public void deleteData(Memory w) {
-
-    }
 
     public void updateUid(String uid) {
         this.uid = uid;
@@ -103,11 +233,31 @@ public class FirebaseHelper {
     */
 
     private void readData(FirestoreCallback firestoreCallback) {
+        myMemories.clear();        // empties the AL so that it can get a fresh copy of data
+        db.collection("users").document(uid).collection("myMemoryList")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot doc: task.getResult()) {
+                                Memory memory = doc.toObject(Memory.class);
+                                myMemories.add(memory);
+                            }
 
+                            Log.i(TAG, "Success reading data: "+ myMemories.toString());
+                            firestoreCallback.onCallback(myMemories);
+                        }
+                        else {
+                            Log.d(TAG, "Error getting documents: " + task.getException());
+                        }
+                    }
+                });
     }
+
 
     //https://stackoverflow.com/questions/48499310/how-to-return-a-documentsnapshot-as-a-result-of-a-method/48500679#48500679
     public interface FirestoreCallback {
-
+        void onCallback(ArrayList<Memory> myList);
     }
 }
